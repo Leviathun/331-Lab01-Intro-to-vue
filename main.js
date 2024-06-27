192bfe65347db1d88227ba41213a9fe8589e5894
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-06-27 21:44:34 +0700

    Change amount of inventory
    change to 9 to get Almost out of Stock

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ createApp({
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
         const inStock = ref(true)
-        const inventory = ref(100)
+        const inventory = ref(1)
         return {
             product ,image ,link ,inStock ,inventory
         }
