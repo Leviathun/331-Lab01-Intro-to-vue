aad7b61e497953215aececa6ef9532a47061a7ee
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-06-27 21:15:30 +0700

    Add conditions and  v-if to check

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ createApp({
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
+        const inStock = ref(true)
         return {
-            product ,image ,link
+            product ,image ,link ,inStock
         }
     }
 
