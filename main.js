b606c40fa6985986fb1778877d92314d09a46f89
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-06-27 21:49:03 +0700

    Add onSale
    boolean that show product is on sale or not

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ createApp({
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
         const inStock = ref(true)
-        const inventory = ref(1)
+        const inventory = ref(100)
+        const onSale = ref(true)
         return {
-            product ,image ,link ,inStock ,inventory
+            product ,image ,link ,inStock ,inventory ,onSale
         }
     }
 
