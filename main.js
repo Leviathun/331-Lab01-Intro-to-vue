7a5315ab4974afeec50933df4d4b1f10c77305e0
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-06-27 21:36:11 +0700

    Change inStock to false

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ createApp({
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
-        const inStock = ref(true)
+        const inStock = ref(false)
         return {
             product ,image ,link ,inStock
         }
